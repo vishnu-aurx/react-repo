59da55e3026eddac598748ac53696b134cc30144
Vishnu Prasad <vishnu.prasad@aurx.co>
2024-08-23 16:05:29 +0530

    added dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Home from './components/Home';
 import Login from './components/Login';
-// import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
@@ -10,8 +10,8 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/login" element={<Login />} />
-                {/* <PrivateRoute path="/dashboard" component={Dashboard} />
-                <Route path="/" element={<Home />} /> */}
+                <PrivateRoute path="/dashboard" component={Dashboard} />
+                {/* <Route path="/" element={<Home />} /> */}
             </Routes>
         </Router>
     );
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+    return (
+        <div>
+            <h2>Dashboard</h2>
+            <p>Welcome to the dashboard! This is a protected route.Login successfully</p>
+        </div>
+    );
+}
+
+export default Dashboard;
